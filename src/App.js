129bb4231fcf55d46b457cf3bc74866c36338002
miss-f-g <fatemeghafari98@gmail.com>
2022-04-07 16:37:42 +0430

    breaking-bad-cast-api

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
+import { CharacterGrid } from "./components/characters/CharacterGrid";
+import Header from "./components/ui/Header";
+import Search from "./components/ui/Search";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
+      // console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, [query]);
+
+  return (
+    <div className="container">
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
+    </div>
+  );
+}
+
+export default App;
